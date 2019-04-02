691c6b6d3a2979a6ad55e182bbe02245acdc17a1
Sean <shonkelli@gmail.com>
2019-04-02 17:53:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+const nestedTarget = function(){
+  return document.querySelector("#nested .target")
+}
+
+const increaseRankBy = function(n){
+  let uls = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < uls.length; i++){
+    let lis = uls[i].children
+    for (let j = 0; j < lis.length; j++){
+      lis[j].innerHTML = parseInt(lis[j].innerHTML) + n
+    }
+  }
+}
+
+const deepestChild = function(){
+  let grandNode = document.getElementById("grand-node")
+  let currentElement = grandNode
+  let children
+  debugger
+  while (currentElement.childElementCount > 0) {
+    children = currentElement.children
+    currentElement = children[0]
+  }
+  return currentElement
+}
